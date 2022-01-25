2130bed12d618005fc44d6957b5f4a99cfac105c
AVAEON\jcassidy <jcassidy@avaeon.com>
2022-01-25 10:23:22 +0000

    start angular

--- apps/demo-angular/src/home.component.ts
+++ apps/demo-angular/src/home.component.ts
@@ -1,58 +1,68 @@
 import { Component } from '@angular/core';
-
+import { DemoSharedFirebaseMessaging } from '@demo/shared';
 @Component({
 	selector: 'demo-home',
 	templateUrl: 'home.component.html',
 })
 export class HomeComponent {
 	demos = [
-	{
-		name: 'firebase-admob'
-	},
-	{
-		name: 'firebase-analytics'
-	},
-	{
-		name: 'firebase-app-check'
-	},
-	{
-		name: 'firebase-auth'
-	},
-	{
-		name: 'firebase-core'
-	},
-	{
-		name: 'firebase-crashlytics'
-	},
-	{
-		name: 'firebase-database'
-	},
-	{
-		name: 'firebase-dynamic-links'
-	},
-	{
-		name: 'firebase-firestore'
-	},
-	{
-		name: 'firebase-functions'
-	},
-	{
-		name: 'firebase-in-app-messaging'
-	},
-	{
-		name: 'firebase-installations'
-	},
-	{
-		name: 'firebase-messaging'
-	},
-	{
-		name: 'firebase-performance'
-	},
-	{
-		name: 'firebase-remote-config'
-	},
-	{
-		name: 'firebase-storage'
+		{
+			name: 'firebase-admob',
+		},
+		{
+			name: 'firebase-analytics',
+		},
+		{
+			name: 'firebase-app-check',
+		},
+		{
+			name: 'firebase-auth',
+		},
+		{
+			name: 'firebase-core',
+		},
+		{
+			name: 'firebase-crashlytics',
+		},
+		{
+			name: 'firebase-database',
+		},
+		{
+			name: 'firebase-dynamic-links',
+		},
+		{
+			name: 'firebase-firestore',
+		},
+		{
+			name: 'firebase-functions',
+		},
+		{
+			name: 'firebase-in-app-messaging',
+		},
+		{
+			name: 'firebase-installations',
+		},
+		{
+			name: 'firebase-messaging',
+		},
+		{
+			name: 'firebase-performance',
+		},
+		{
+			name: 'firebase-remote-config',
+		},
+		{
+			name: 'firebase-storage',
+		},
+	];
+	demo: DemoSharedFirebaseMessaging;
+	ngOnInit(): void {
+		/* ***********************************************************
+		 * Use the "ngOnInit" handler to initialize data for this component.
+		 *************************************************************/
+
+		console.info('ngOnInit');
+		this.demo = new DemoSharedFirebaseMessaging();
+		this.demo.testIt();
 	}
-];
 }
--- apps/demo-angular/src/main.ts
+++ apps/demo-angular/src/main.ts
@@ -1,6 +1,62 @@
-import { runNativeScriptAngularApp, platformNativeScript } from '@nativescript/angular';
+import { runNativeScriptAngularApp, platformNativeScript, NativeScriptNgZone } from '@nativescript/angular';
 import { AppModule } from './app.module';
 
+import { firebase } from '@nativescript/firebase-core';
+
+console.log('*********Atempt to init firebase************');
+
+firebase().initializeApp();
+const messaging = firebase().messaging();
+
+messaging.onToken((token) => {
+	console.log('[Firebase] onPushTokenReceivedCallback:', { token });
+});
+
+messaging.onMessage((message) => {
+	console.log('Firebase onMessage', JSON.stringify(message));
+});
+
+messaging.onNotificationTap((message) => {
+	console.log('Firebase onNotificationTap', message);
+});
+
+messaging
+	.requestPermission({
+		ios: {
+			alert: true,
+			announcement: undefined,
+			badge: true,
+			carPlay: undefined,
+			criticalAlert: undefined,
+			provisional: undefined,
+			sound: true,
+		},
+	})
+	.then((authStatus) => {
+		console.log('NotificationPermission', 'done', authStatus);
+		messaging
+			.registerDeviceForRemoteMessages()
+			.then(() => {
+				console.log('Firebase Device Registered');
+
+				messaging
+					.getToken()
+					.then((token) => {
+						console.log('[Firebase] Got the token:', { token });
+					})
+					.catch((e) => {
+						console.error('FirebaseGetTokenError', e);
+					});
+				//messaging.showNotificationsWhenInForeground = true;
+			})
+			.catch((e) => {
+				console.error('registerDeviceForRemoteMessages', e);
+			});
+	})
+	.catch((e) => {
+		console.error('requestPermission', e);
+	});
+
 runNativeScriptAngularApp({
-  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
+	appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule, { ngZone: new NativeScriptNgZone() }),
 });
